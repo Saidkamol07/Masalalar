09acd764be0124de134480c7b5988c8e6b90e248
Saidkamol07 <lasiddiasov@gmail.com>
2024-06-21 02:12:33 +0500

    Masalalar 24tasi

---
+++ JS/main.js
@@ -0,0 +1,210 @@
+// 1
+
+// let a = 'string';
+// let b = 'soz';
+
+// if (a && b) {
+// 	console.log('Ikkalasi ham true');
+// }
+
+// 2
+
+// let a = 'string';
+// let b = '';
+
+// if (a || b) {
+// 	console.log('Ikkalasi ham true');
+// }
+
+// 3
+
+// let a = '0';
+
+// if (a == false) {
+// 	console.log('a false');
+// }
+
+// 4
+
+// let a = 'string';
+// let b = 'soz';
+// let c = 10;
+
+// if (a || b === true) {
+// 	console.log(c);
+// }
+
+// 5
+
+// let a = 'string';
+// let b = 0;
+// if (a || b == false) {
+// 	console.log('Kamida bittasi false');
+// }
+
+// 6
+
+// let x = 10;
+// let y = 10;
+
+// if (x && y == 10) {
+// 	console.log('Sonlar teng');
+// }
+
+// 7
+
+// let x = 11;
+// let y = 3;
+
+// if ((x > 10, y < 5)) {
+// 	console.log('Shart bajarildi');
+// }
+
+// 8
+
+// let age = 20;
+// if (age > 18) {
+// 	console.log('Balogatga yetgan');
+// }
+
+// 9
+
+// let son = 3;
+
+// if ((son = 3)) {
+// 	console.log('Toq son');
+// }
+
+// 10
+
+// let number = 99;
+
+// if (number > 100) {
+// 	console.log('Musbat va kichik');
+// }
+
+// 11
+
+// let x = 5;
+// let y = 4;
+
+// if (x || y) {
+// 	console.log('Sonlar teng emas');
+// }
+
+// 12
+
+// let a = 'string';
+// let b = 0;
+
+// if (a || b) {
+// 	console.log('Kamida biri false');
+// }
+
+// 13
+
+// let son = -10;
+
+// if (son) {
+// 	console.log('Manfiy son');
+// }
+
+// 14
+
+// let x = 4;
+// let y = 6;
+// if (x && y) {
+// 	console.log('Ikkalasi ham juft');
+// }
+
+// 15
+
+// let isMember = 'null';
+// let age = 20;
+// if ((isMember === true, age > 18)) {
+// 	console.log('Xush kelibsiz, azo!');
+// }
+
+// 16
+
+// let a = 10;
+// let b = 10;
+// let c = 22;
+
+// if (a === b || c) {
+// 	console.log('Shartlardan biri bajarildi');
+// }
+
+// 17
+
+// let isValid = true;
+
+// if (isValid === true) {
+// 	console.log('Valid');
+// }
+
+// 18
+
+// let temp = -11;
+
+// if (temp < 0) {
+// 	console.log('Sovuq');
+// }
+
+// 19
+
+// let score = 60;
+// let isPassed = true;
+
+// if (score > 50 && isPassed === true) {
+// 	console.log('Otdi');
+// }
+
+// 20
+
+// let isOnline = true;
+// let isAvaible = true;
+
+// if (isOnline && isAvaible === true) {
+// 	console.log('Ulanish mumkin');
+// }
+
+// 21
+
+// let num = 3;
+
+// if (num < 10 && 1 < num) {
+// 	console.log('Diapazonda');
+// }
+
+// 22
+
+// let isWeekend = 0;
+
+// if (isWeekend == false) {
+// 	console.log('Ish kuni');
+// }
+
+// 23
+
+// let a = 10;
+// let b = 6;
+
+// if (a || b == true) {
+// 	console.log('Kamida biri true');
+// }
+
+// 24
+
+// let age = 16;
+
+// if ((age > 13, age < 19)) {
+// 	console.log('Teenager');
+// }
+
+// for (let u = 1; u < 21; u++) {
+// 	if (u === 1) {
+// 		continue;
+// 	}
+// 	console.log(u);
+// }
