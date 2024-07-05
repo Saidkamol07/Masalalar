6846b180aae1fdb6fd9ccd03050130cbdc63cdf4
Saidkamol07 <lasiddiasov@gmail.com>
2024-07-05 15:40:14 +0500

    return function

--- JS/main.js
+++ JS/main.js
@@ -1,68 +1,10 @@
-// 1
-// function add() {
-// 	console.log('Salom Dunyo');
-// }
-// add();
+function add(x, y) {
+	return x + y;
+}
 
-// 2
+console.log(add(5, 10));
 
-// let a = (a) => {
-// 	console.log(a ** 2);
-// }
-
-// a(4);
-
-// 3
-// let w = (a, b) => {
-// 	console.log(a + b);
-// }
-
-// w(22, 33);
-
-// 4
-
-// let i = (ism = 'Saidkamol') => {
-// 	console.log(`Salom ${ism}`);
-// };
-
-// i();
-
-// 5
-
-// let q = (a) => {
-// 	console.log(a ** 3);
-// }
-
-// q(2);
-
-// 6
-
-// let a = (a, b) => {
-// 	console.log(a * b);
-// };
-
-// a(2, 4);
-
-// 7
-
-// let i = (ism = 'Saidkamol') => {
-// 	console.log(`Salom ${ism}`);
-// };
-
-// i();
-
-// 8
-
-// let x = (a, b) => {
-// 	console.log(a | b);
-// };
-
-// x(4, 2);
-
-10;
-let c = a => {
-	let i = toString(a);
-	console.log(typeof i);
-};
-
-c(25);
+const a = add(2, 3);
+if (a === 5) {
+	console.log('Teng');
+}
