ca58ae0f73478a2f14e8a276752583d93f9dc0bb
Saidkamol07 <lasiddiasov@gmail.com>
2024-07-05 14:57:49 +0500

    added other exersises

--- JS/main.js
+++ JS/main.js
@@ -1,210 +1,68 @@
 // 1
-
-// let a = 'string';
-// let b = 'soz';
-
-// if (a && b) {
-// 	console.log('Ikkalasi ham true');
+// function add() {
+// 	console.log('Salom Dunyo');
 // }
+// add();
 
 // 2
 
-// let a = 'string';
-// let b = '';
-
-// if (a || b) {
-// 	console.log('Ikkalasi ham true');
+// function add(a) {
+// 	console.log(a ** 2);
 // }
+
+// add(4);
 
 // 3
-
-// let a = '0';
-
-// if (a == false) {
-// 	console.log('a false');
+// function add(a, b) {
+// 	console.log(a + b);
 // }
+
+// add(22, 33);
 
 // 4
 
-// let a = 'string';
-// let b = 'soz';
-// let c = 10;
+// let i = (ism = 'Saidkamol') => {
+// 	console.log(`Salom ${ism}`);
+// };
 
-// if (a || b === true) {
-// 	console.log(c);
-// }
+// i();
 
 // 5
 
-// let a = 'string';
-// let b = 0;
-// if (a || b == false) {
-// 	console.log('Kamida bittasi false');
+// function add(a) {
+// 	console.log(a ** 3);
 // }
+
+// add(2);
 
 // 6
 
-// let x = 10;
-// let y = 10;
+// const a = function add(a, b) {
+// 	console.log(a * b);
+// };
 
-// if (x && y == 10) {
-// 	console.log('Sonlar teng');
-// }
+// a(2, 4);
 
 // 7
 
-// let x = 11;
-// let y = 3;
+// let i = (ism = 'Saidkamol') => {
+// 	console.log(`Salom ${ism}`);
+// };
 
-// if ((x > 10, y < 5)) {
-// 	console.log('Shart bajarildi');
-// }
+// i();
 
 // 8
 
-// let age = 20;
-// if (age > 18) {
-// 	console.log('Balogatga yetgan');
-// }
+// let x = (a, b) => {
+// 	console.log(a | b);
+// };
 
-// 9
+// x(4, 2);
 
-// let son = 3;
+10;
+function add(a) {
+	let i = toString(a);
+	console.log(typeof i);
+}
 
-// if ((son = 3)) {
-// 	console.log('Toq son');
-// }
-
-// 10
-
-// let number = 99;
-
-// if (number > 100) {
-// 	console.log('Musbat va kichik');
-// }
-
-// 11
-
-// let x = 5;
-// let y = 4;
-
-// if (x || y) {
-// 	console.log('Sonlar teng emas');
-// }
-
-// 12
-
-// let a = 'string';
-// let b = 0;
-
-// if (a || b) {
-// 	console.log('Kamida biri false');
-// }
-
-// 13
-
-// let son = -10;
-
-// if (son) {
-// 	console.log('Manfiy son');
-// }
-
-// 14
-
-// let x = 4;
-// let y = 6;
-// if (x && y) {
-// 	console.log('Ikkalasi ham juft');
-// }
-
-// 15
-
-// let isMember = 'null';
-// let age = 20;
-// if ((isMember === true, age > 18)) {
-// 	console.log('Xush kelibsiz, azo!');
-// }
-
-// 16
-
-// let a = 10;
-// let b = 10;
-// let c = 22;
-
-// if (a === b || c) {
-// 	console.log('Shartlardan biri bajarildi');
-// }
-
-// 17
-
-// let isValid = true;
-
-// if (isValid === true) {
-// 	console.log('Valid');
-// }
-
-// 18
-
-// let temp = -11;
-
-// if (temp < 0) {
-// 	console.log('Sovuq');
-// }
-
-// 19
-
-// let score = 60;
-// let isPassed = true;
-
-// if (score > 50 && isPassed === true) {
-// 	console.log('Otdi');
-// }
-
-// 20
-
-// let isOnline = true;
-// let isAvaible = true;
-
-// if (isOnline && isAvaible === true) {
-// 	console.log('Ulanish mumkin');
-// }
-
-// 21
-
-// let num = 3;
-
-// if (num < 10 && 1 < num) {
-// 	console.log('Diapazonda');
-// }
-
-// 22
-
-// let isWeekend = 0;
-
-// if (isWeekend == false) {
-// 	console.log('Ish kuni');
-// }
-
-// 23
-
-// let a = 10;
-// let b = 6;
-
-// if (a || b == true) {
-// 	console.log('Kamida biri true');
-// }
-
-// 24
-
-// let age = 16;
-
-// if ((age > 13, age < 19)) {
-// 	console.log('Teenager');
-// }
-
-// for (let u = 1; u < 21; u++) {
-// 	if (u === 1) {
-// 		continue;
-// 	}
-// 	console.log(u);
-// }
+add(25);
