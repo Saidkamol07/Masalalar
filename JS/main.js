cf32e3563b8e393a6a14f260c7152ca70e8e7d8b
Saidkamol07 <lasiddiasov@gmail.com>
2024-07-12 01:34:48 +0500

    add new masalalar

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,68 @@
 
 // let a = (a, b, c) => a + b + c;
 // console.log(a(4, 5, 7));
+
+// function ism(ism, fam) {
+// 	console.log(`Mening ismim: ${ism}`);
+// 	console.log(`Mening familiyam: ${fam}`);
+// }
+
+// ism('Saidkamol', 'Mukhammadjanov');
+
+// const ism = function (ism = 'Sean', fam = 'Smith') {
+// 	console.log(`Mening ismim: ${ism}`);
+// 	console.log(`Mening familiyam: ${fam}`);
+// };
+
+// ism('Saidkamol', 'Mukhammadjanov');
+
+// ism();
+
+// For Each
+
+// let a = ['Null', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'String', 'Saidkamol'];
+// a.forEach(function (i, index, array) {
+// 	console.log('Bu osha array:', array);
+// 	// console.log('Bu osha i:', i);
+// 	// console.log('Bu osha index:', index);
+// });
+
+// const b = [1, 2, 3, 4, 5, 6, , 7, 8, 9, 10];
+// b.forEach(function (i) {
+// 	console.log(i ** 3);
+// });
+
+// Object
+// const object = {
+// 	Name: 'Saidkamol',
+// 	lastName: 'Mukhammadjanov',
+// 	Age: 16,
+// 	Country: 'Uzbekistan',
+// 	City: 'Andijan',
+// 	Hobby: 'Watching Movies',
+// };
+
+// console.log(object);
+// console.log(object.Age);
+// console.log(object.City);
+// console.log(object.Country);
+// console.log(object.Hobby);
+// console.log(object.Name);
+// console.log(object.lastName);
+// console.log(Object.keys(object));
+// console.log(Object.values(object));
+
+// Math
+
+// console.log(Math.floor(7.3));
+// console.log(Math.ceil(7.9));
+// console.log(Math.round(3.4));
+// console.log(Math.abs(7.3));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(4, 2));
+// console.log(Math.random(0, 1));
+// console.log(Math.random(0) ** 10);
+// console.log(Math.PI);
+// console.log(Math);
+// console.log(Math.max(1, 10));
+// console.log(Math.min(22, -11));
